5bb51775b7ab03d56ebbd0f587f4c639c031d151
le.trong <le.trong>
2021-07-26 01:45:28 -0400

    Added logic to map special ticker to yahoo finance

--- consts/app.const.go
+++ consts/app.const.go
@@ -11,3 +11,205 @@ const (
 	SECURITY_TYPE = "EQUITY"
 	DATA_SOURCE   = "TIP_RANK"
 )
+
+var TipRankToYahooTickers = []struct {
+	TipRank string
+	Yahoo   string
+}{
+	{
+		TipRank: "TSE:LPC",
+		Yahoo:   "LPC.V",
+	},
+	{
+		TipRank: "TSE:SJR.A",
+		Yahoo:   "SJR-A.V",
+	},
+	{
+		TipRank: "TSE:NET.UN",
+		Yahoo:   "NET-UN.V",
+	},
+	{
+		TipRank: "TSE:IEI",
+		Yahoo:   "IEI.V",
+	},
+	{
+		TipRank: "TSE:DE",
+		Yahoo:   "DE.V",
+	},
+	{
+		TipRank: "TSE:FCD.UN",
+		Yahoo:   "FCD-UN.V",
+	},
+	{
+		TipRank: "TSE:AFCC.H",
+		Yahoo:   "AFCC-H.V",
+	},
+	{
+		TipRank: "TSE:VCI",
+		Yahoo:   "VCI.V",
+	},
+	{
+		TipRank: "TSE:CMI",
+		Yahoo:   "CMI.V",
+	},
+	{
+		TipRank: "TSE:FDI",
+		Yahoo:   "FDI.V",
+	},
+	{
+		TipRank: "TSE:FCA.UN",
+		Yahoo:   "FCA-UN.V",
+	},
+	{
+		TipRank: "TSE:INP",
+		Yahoo:   "INP.V",
+	},
+	{
+		TipRank: "TSE:ORC.B",
+		Yahoo:   "ORC-B.V",
+	},
+	{
+		TipRank: "TSE:ORC.A",
+		Yahoo:   "ORC-A.V",
+	},
+	{
+		TipRank: "TSE:TII",
+		Yahoo:   "TII.V",
+	},
+	// {
+	// 	TipRank: "TSE:FRO.UN",
+	// 	Yahoo:   "FRO-UN.V",
+	// },
+	{
+		TipRank: "TSE:AWI",
+		Yahoo:   "AWI.V",
+	},
+	{
+		TipRank: "TSE:WBE",
+		Yahoo:   "WBE.V",
+	},
+	{
+		TipRank: "TSE:RZZ",
+		Yahoo:   "RZZ.V",
+	},
+	{
+		TipRank: "TSE:NXLV",
+		Yahoo:   "NXLV.V",
+	},
+	{
+		TipRank: "TSE:ITM",
+		Yahoo:   "ITM.V",
+	},
+	{
+		TipRank: "TSE:MHC.USD",
+		Yahoo:   "MHC-U.TO",
+	},
+	{
+		TipRank: "TSE:SFI",
+		Yahoo:   "SFI.V",
+	},
+	{
+		TipRank: "TSE:TTR",
+		Yahoo:   "TTR.V",
+	},
+	{
+		TipRank: "TSE:NWX",
+		Yahoo:   "NWX.V",
+	},
+	{
+		TipRank: "TSE:MARV",
+		Yahoo:   "MARV.V",
+	},
+	{
+		TipRank: "TSE:ARD",
+		Yahoo:   "ARD.V",
+	},
+	{
+		TipRank: "TSE:QCA",
+		Yahoo:   "QCA.CN",
+	},
+	{
+		TipRank: "TSE:RE",
+		Yahoo:   "RE.V",
+	},
+	{
+		TipRank: "TSE:TLP.UN",
+		Yahoo:   "TLP-UN.CN",
+	},
+	{
+		TipRank: "TSE:GTWO",
+		Yahoo:   "GTWO.V",
+	},
+	{
+		TipRank: "TSE:BCF",
+		Yahoo:   "BCF.V",
+	},
+	{
+		TipRank: "TSE:UFC",
+		Yahoo:   "UFC.V",
+	},
+	{
+		TipRank: "TSE:SVI",
+		Yahoo:   "SVI.V",
+	},
+	{
+		TipRank: "TSE:CTO",
+		Yahoo:   "CTO.V",
+	},
+	{
+		TipRank: "TSE:AAN",
+		Yahoo:   "AAN.V",
+	},
+	{
+		TipRank: "TSE:ELC",
+		Yahoo:   "ELC.V",
+	},
+	{
+		TipRank: "TSE:AMK",
+		Yahoo:   "AMK.V",
+	},
+	{
+		TipRank: "TSE:NVV",
+		Yahoo:   "NVV.V",
+	},
+	{
+		TipRank: "TSE:MCS",
+		Yahoo:   "MCS.V",
+	},
+	{
+		TipRank: "TSE:JTC",
+		Yahoo:   "JTC.V",
+	},
+	{
+		TipRank: "TSE:RFC",
+		Yahoo:   "RFC.V",
+	},
+	{
+		TipRank: "TSE:CSL",
+		Yahoo:   "CSL.V",
+	},
+	{
+		TipRank: "TSE:RBX",
+		Yahoo:   "RBX.V",
+	},
+	{
+		TipRank: "TSE:EFH",
+		Yahoo:   "EFH.V",
+	},
+	{
+		TipRank: "TSE:AMMO",
+		Yahoo:   "AMMO.V",
+	},
+	{
+		TipRank: "TSE:AG.UN",
+		Yahoo:   "AG-UN.CN",
+	},
+	{
+		TipRank: "TSE:SNF",
+		Yahoo:   "SNF.V",
+	},
+	{
+		TipRank: "TSE:SHL",
+		Yahoo:   "SHL.V",
+	},
+}
--- utils/ticker/ticker.util.go
+++ utils/ticker/ticker.util.go
@@ -3,10 +3,17 @@ package ticker
 import (
 	"fmt"
 	"strings"
+
+	"github.com/hthl85/aws-tiprank-norm-list/consts"
 )
 
 // GenYahooTickerFromTipRankTicker gen yahoo ticker from tip rank ticker
 func GenYahooTickerFromTipRankTicker(tiprankTicker string) string {
+	specialTicker := getSpecialYahooTicker(tiprankTicker)
+	if specialTicker != "" {
+		return specialTicker
+	}
+
 	parts := strings.Split(tiprankTicker, ":")
 
 	if len(parts) > 1 {
@@ -16,4 +23,15 @@ func GenYahooTickerFromTipRankTicker(tiprankTicker string) string {
 	}
 
 	return fmt.Sprintf("%s.TO", tiprankTicker)
+}
+
+// getSpecialYahooTicker gets special yahoo ticker from a given tiprank ticker
+func getSpecialYahooTicker(tiprankTicker string) string {
+	for _, ticker := range consts.TipRankToYahooTickers {
+		if strings.EqualFold(strings.ToUpper(ticker.TipRank), strings.ToUpper(tiprankTicker)) {
+			return ticker.Yahoo
+		}
+	}
+
+	return ""
 }
